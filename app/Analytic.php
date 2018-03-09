9fcf6d5ab4f8069660a4d1fa53354f864957c27b
carycha <carycha@gmail.com>
2018-03-09 18:14:12 +0800

    change custom page to be analytics

--- app/Analytic.php
+++ app/Analytic.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 
-class Testdata extends Model
+class Analytic extends Model
 {
     
 }
--- app/Http/Controllers/Voyager/AnalyticsController.php
+++ app/Http/Controllers/Voyager/AnalyticsController.php
@@ -5,17 +5,17 @@ namespace App\Http\Controllers\Voyager;
 use TCG\Voyager\Http\Controllers\VoyagerBreadController as BaseVoyagerBreadController;
 use App\Testdata;
 use Illuminate\Http\Request;
+use App\Customer;
 
-class TestdatasController extends BaseVoyagerBreadController
+class AnalyticsController extends BaseVoyagerBreadController
 {
     //
     public function index(Request $request) {
-#        $testdatas = App\Testdata::All();
-        $view = 'vendor.voyager.testdatas.browse';
+        $view = 'vendor.voyager.analytics.browse';
 #        return Voyager::view($view, ['list' => $sales]);
         #$name = "Carycha";
-        #$testdatas = Testdata::All();
-        $testdatas = Testdata::All()->groupby('age');
+        #$testdatas = Testdata::All()->groupby('age');
+        $testdatas = Customer::All();
 
 #        return \TCG\Voyager\Facades\Voyager::view($view, compact("name", $name));
         return View($view, compact("testdatas", $testdatas));
